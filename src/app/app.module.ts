86c2ba6b999ee965f0aaed8e5a7c228f1d289a94
nir <nir@panaya.com>
2017-08-27 21:58:25 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,38 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule, Routes }   from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+
+const appRoutes: Routes = [
+	  {path: '', 	redirectTo: '/dashboard', pathMatch: 'full'},
+	  {path: 'heroes', component: HeroesComponent},
+  	{path: 'dashboard',	component: DashboardComponent},
+  	{path: 'detail/:id', component: HeroDetailComponent},
+];
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent   
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
